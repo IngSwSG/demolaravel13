96e07acefc27dfe81223cd96ba38af9cb9c20597
AustinPonce <carrilloponce11@gmail.com>
2026-06-03 16:36:47 -0600

    Fix complete task route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,5 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('tasks', \App\Http\Controllers\TaskController::class);
+Route::apiResource('tasks', TaskController::class);
 Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete']);
