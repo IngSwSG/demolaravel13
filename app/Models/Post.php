2c8d75d04b08bc72c103335b8f5aac3631982d62
yurimdz <YURIDIA.MENDOZA@ucr.ac.cr>
2026-06-05 15:09:12 -0600

    feat: implementar Post, Like y migraciones para pasar LikesTest

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'body', 'user_id'];
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function like()
+    {
+        $this->likes()->create([
+            'user_id' => auth()->id(),
+        ]);
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title'   => fake()->sentence(),
+            'body'    => fake()->paragraph(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
