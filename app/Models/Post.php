ea34784e3b25e0833c52610f73b062e0fdb588e4
Z7VENS <167641448+Z7VENS@users.noreply.github.com>
2026-06-05 14:27:10 -0600

    feat: LikesTest

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory; 
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function like()
+    {
+        return $this->likes()->create([
+            'user_id' => auth()->id(),
+        ]);
+    }
+}
