1793e6cbbd91188f4f99f1156c0624d05ec4d064
Ángee Rod <angelesrb2701@gmail.com>
2026-06-05 14:29:39 -0600

    Ejercicio 2 - C36641 y C07126
    - Agregar modelo Post con metodo like() y relacion polimorfica
    - Agregar modelo Like con relacion morfica
    - Agregar migracion para tabla posts
    - Agregar migracion para tabla likes (polimorfica)
    - Agregar PostFactory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'body' => fake()->paragraph(),
+        ];
+    }
+}
